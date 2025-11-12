9dcd0693d3b12f32cf6152dbb3db0b31cc8e1336
bmoreto <beatriz_moreto@hotmail.com>
2025-11-12 09:02:53 -0300

    fecth data

--- src/fetchData.ts
+++ src/fetchData.ts
@@ -1,11 +1,11 @@
-export default async function fetchData(url: string){
-  try {
-    const response =  await fetch(url);
-        console.log(response);
-    if(!response.ok) throw new Error ("Erro: " + response.status);
+export default async function fetchData<T>(url:string): Promise<T | null>{
+  try{
+    const response = await fetch(url);
+    if(!response.ok) throw new Error("Erro: " + response.status);
     const json = await response.json();
-    return json;  
-  } catch (error) {
+    return json;
+  }catch(error){
+    if(error instanceof Error) console.log("fetchData: " + error.message);
     return null;
   }
 }
--- src/script.ts
+++ src/script.ts
@@ -1,11 +1,26 @@
 import fetchData from "./fetchData.js";
 
-async function handleData(){
-  const data = await fetchData('https://api.origamid.dev/json/transacoes.json');
+type TransacaoPagamento = "Boleto" | "Cartão de Crédito";
+type TransacaoStatus = "Paga" | "Recusada pela operadora de cartão" | "Aguardando pagamento" | "Estornado";
 
-data.forEach((item) => {
-  console.log(item)
-});
+interface TransacaoAPI{
+  Nome: string;
+  ID: number;
+  Data: string;
+  Status: TransacaoStatus;
+  Email: string;
+  ['Valor (R$)']: string;
+  ['Forma de Pagamento']: TransacaoPagamento;
+  ['Cliente Novo']: number;
+}
+
+async function handleData(){
+  const data = await fetchData<TransacaoAPI[]>('https://api.origamid.dev/json/transacoes.json');
+  if(data){
+  data.forEach((item) => {
+  console.log(item["Valor (R$)"]);
+  });
+  }
 console.log('codigo continuou');
 }
 
